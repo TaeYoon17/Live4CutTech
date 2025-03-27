520d095b54da836a2f476e0f49b30c624b68cda2
TaeYoon17 <46375289+TaeYoon17@users.noreply.github.com>
2025-03-27 12:36:27 +0900

    [Fix] vImage DownSample 적용

--- LiveFourCut/LiveFourCut/Services/ExtractService.swift
+++ LiveFourCut/LiveFourCut/Services/ExtractService.swift
@@ -8,6 +8,7 @@
 import Foundation
 import AVFoundation
 import CoreImage
+import Accelerate
 enum ExtractError: Error{
     case emptyContainer
 }
@@ -20,7 +21,7 @@ class ExtractService{
         guard !avAssetContainers.isEmpty else { throw ExtractError.emptyContainer }
         let imageDatas: [[CGImage]] = try await withThrowingTaskGroup(of: (Int,[CGImage]).self) { taskGroup in
             for (offset,v) in avAssetContainers.enumerated(){
-                taskGroup.addTask {[minDuration,fps] in
+                taskGroup.addTask {[self, minDuration,fps] in
                     let asset = AVAsset(url: URL(string: v.originalAssetURL)!)
                     let generator = AVAssetImageGenerator(asset: asset)
                     generator.appliesPreferredTrackTransform = true
@@ -30,14 +31,16 @@ class ExtractService{
                     var lastImage: CGImage!
                     let time = CMTime(seconds: 0, preferredTimescale: 600)
                     let imgContain = try await generator.image(at: time)
-                    imageDatas.append(imgContain.image)
-                    lastImage = imgContain.image
+                    let downImage = self.downsampleVImage(image: imgContain.image)
+                    imageDatas.append(downImage)
+                    lastImage = downImage
                     for idx in (1..<Int(minDuration * fps)){
                         let time = CMTime(seconds: Double(idx) / 24, preferredTimescale: 600)
                         let imgContain = try? await generator.image(at: time)
-                        if let imgContain{ 
-                            imageDatas.append(imgContain.image)
-                            lastImage = imgContain.image
+                        if let imgContain{
+                            let downImage = self.downsampleVImage(image: imgContain.image)
+                            imageDatas.append(downImage)
+                            lastImage = downImage
                         }
                         else{ imageDatas.append(lastImage) }
                     }
@@ -49,5 +52,63 @@ class ExtractService{
             return imageContainers
         }
         return imageDatas
+    }
+}
+extension ExtractService {
+    func downSample(image: CGImage) -> CGImage {
+        
+        let ciImage = CIImage(cgImage: image)
+        let targetWidth : CGFloat = 360
+        let scale = targetWidth / ciImage.extent.width
+        let targetHeight = ciImage.extent.height * scale
+        let scaleSize = CGSize(width: targetWidth, height: targetHeight)
+        let transformedCIImage = ciImage.transformed(by: .init(scaleX: scale, y: scale), highQualityDownsample: true)
+        let context = CIContext()
+        let afterDownsamplingImage = context.createCGImage(transformedCIImage, from: .init(origin: .zero, size: scaleSize))!
+        
+        return afterDownsamplingImage
+    }
+}
+extension ExtractService {
+    func downsampleVImage(image: CGImage, targetSize: CGSize = .init(width: 300, height: 300*1.77)) -> CGImage {
+        guard let format = vImage_CGImageFormat(
+            bitsPerComponent: 8,
+            bitsPerPixel: 32,
+            colorSpace: image.colorSpace ?? CGColorSpaceCreateDeviceRGB(),
+            bitmapInfo: CGBitmapInfo(rawValue: CGImageAlphaInfo.noneSkipLast.rawValue),
+            renderingIntent: .defaultIntent
+        ) else {
+            return image
+        }
+        do {
+            var sourceBuffer = try vImage_Buffer(cgImage: image, format: format)
+            var destinationBuffer = vImage_Buffer()
+            defer {
+                free(destinationBuffer.data)
+                free(sourceBuffer.data)
+            }
+            // 이미지 크기 조절 알고리즘
+            let scaleX = targetSize.width / CGFloat(image.width)
+            let scaleY = targetSize.height / CGFloat(image.height)
+            let scale = min(scaleX, scaleY)
+            let destWidth = Int(CGFloat(image.width) * scale)
+            let destHeight = Int(CGFloat(image.height) * scale)
+            
+            let bytesPerPixel = 4 // rgba를 대응하기 1바이트당 하나씩
+            destinationBuffer.width = UInt(destWidth)
+            destinationBuffer.height = UInt(destHeight)
+            destinationBuffer.rowBytes = destWidth * bytesPerPixel // 한 열의 bytes
+            destinationBuffer.data = malloc(destHeight * destinationBuffer.rowBytes) // 데이터 할당한다.
+            
+            // openCV에서 scaling을 해주는 것과 비슷한 메서드
+            vImageScale_ARGB8888(&sourceBuffer, &destinationBuffer, nil, vImage_Flags(kvImageHighQualityResampling))
+            
+            let destCGImage = try destinationBuffer.createCGImage(format: format)
+            print("Prev Downsampling image data size ", image.width * image.height * 4 / 1024)
+            print("After Downsampling image data size ", destCGImage.width * destCGImage.height * 4 / 1024)
+            return destCGImage
+        } catch {
+            return image
+        }
     }
 }
